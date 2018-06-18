9bceadbba4ce7950927c4ec439c57cd9a62f8d27
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-06-18 12:19:47 -0400

    Update project description and URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jesture "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Experimental visualization of clojure.spec."
+  :url "https://github.com/sgepigon/jesture"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]]
