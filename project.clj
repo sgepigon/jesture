9bf2adcdc88641048d619c9d8108559751b788ca
Santiago Gepigon <santiago.gepigon@gmail.com>
2018-06-18 12:32:09 -0400

    Add quil as a dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/sgepigon/jesture"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [quil "2.7.1"]]
   :main ^:skip-aot jesture.core
   :target-path "target/%s"
   :profiles {:dev {:source-paths ["dev"]
